adcd2db6c33ca5099a35ba2165e1fc42bec75dcf
sitrongtrang <sitrongtrang@gmail.com>
2025-04-09 11:51:26 +0700

    Update vietnamese_language_processing.py

--- components/language_processing/impl/vietnamese_language_processing.py
+++ components/language_processing/impl/vietnamese_language_processing.py
@@ -58,9 +58,9 @@ class VietnameseLanguageProcessing(LanguageProcessing):
     
     def text_preprocessing(self, text):
         tokenized_text = self._text_preprocessing(text)
-        tokenized_text = [tokenized_sent.split(' ') for tokenized_sent in tokenized_text]
-        return [token.replace("_", " ") for tokenized_sent in tokenized_text for token in tokenized_sent]
-        # return tokenized_text
+        # tokenized_text = [tokenized_sent.split(' ') for tokenized_sent in tokenized_text]
+        # return [token.replace("_", " ") for tokenized_sent in tokenized_text for token in tokenized_sent]
+        return tokenized_text
 
         
     def pre_trained_tokenizer_model(self):
